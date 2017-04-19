53d8f2614f3fa89f21a8a395072a161f97bdc9b2
Felix Holzwarth <felixholzwarth@Felixs-MBP.fritz.box>
2017-04-19 22:36:15 +0200

    some chnages

--- Set2.hs
+++ Set2.hs
@@ -1,10 +1,10 @@
 module Set2 where
 
-import MCPrelude()
-import Set1()
+import MCPrelude
+
 
 data Maybe a = Just a | Nothing
 
 instance Show a => Show (Set2.Maybe a) where
   show Just a  = "a"
-  show Nothing = "Nothing "
+  show Nothing = "Nothing"
