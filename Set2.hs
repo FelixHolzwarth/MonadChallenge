b6eca08def04931a4589713edea1e7dab876667a
Felix Holzwarth <felixholzwarth@Felixs-MBP.fritz.box>
2017-04-19 22:48:18 +0200

    implemented headMay

--- Set2.hs
+++ Set2.hs
@@ -11,3 +11,7 @@ data Maybe a = Just a | Nothing
 instance Show a => Show (Maybe a) where
   show (Just a)  = "Just " ++ show a
   show Nothing = "Nothing"
+
+headMay :: [a] -> Maybe a
+headMay (x:_) = Just x
+headMay []     = Nothing
