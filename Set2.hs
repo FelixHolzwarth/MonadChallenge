46cabfa6bfcb571c3a69c558e6121c4798e9cf3b
Felix Holzwarth <felixholzwarth@Felixs-MBP.fritz.box>
2017-04-20 21:42:10 +0200

    implemented lookupMay

--- Set2.hs
+++ Set2.hs
@@ -19,3 +19,9 @@ headMay []     = Nothing
 tailMay :: [a] -> Maybe [a]
 tailMay (_:xs) = Just xs
 tailMay []     = Nothing
+
+lookupMay :: Eq a => a -> [(a, b)] -> Maybe b
+lookupMay elem lst = case filter (\(a,b) -> elem == a) lst of
+  [(a,b)]   -> Just b 
+  [(a,b),_] -> Just b
+  otherwise -> Nothing
